4ed5c8336bfb82164da814f029d284f0161ecc4f
Ashutosh <30094761+agojiya@users.noreply.github.com>
2018-07-21 22:28:21 -0400

    Progress printing for image conversion

--- io/prepare_data.py
+++ io/prepare_data.py
@@ -1,13 +1,27 @@
 from os import listdir
 from os.path import isfile, join
+from math import floor
 
 
 def convert_to_grayscale(color_dir, grayscale_dir):
     import cv2
 
-    images = [file for file in listdir(color_dir) if
+    if len(listdir(grayscale_dir)) != 0:
+        return
+
+    color_files = listdir(color_dir)
+    interval = floor(len(color_files) / 10)
+
+    print("Converting training images into grayscale", end='')
+    counter = 0
+    images = [file for file in color_files if
               isfile(join(color_dir, file))]
     for image_file in images:
         image = cv2.imread(join(color_dir, image_file))
         image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         cv2.imwrite(join(grayscale_dir, image_file), image)
+
+        counter += 1
+        if counter % interval == 0:
+            print('.', end='')
+    print('DONE')
