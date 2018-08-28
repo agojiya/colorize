a3b428c768858c6e7f965fae661520eab9a4b460
Ashutosh <30094761+agojiya@users.noreply.github.com>
2018-08-28 13:23:50 -0400

    Setting up for a GAN model

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from fileio import prepare_data
 from os import path
 
+import tensorflow as tf
+
 BASE_DIR = path.join('X:', 'open-images-v4')
 TRAIN_COLOR_DIR = path.join(BASE_DIR, 'train')
 TRAIN_DIR = path.join(BASE_DIR, 'train_grayscale')
@@ -9,3 +11,7 @@ prepare_data.convert_to_grayscale(color_dir=TRAIN_COLOR_DIR,
                                   grayscale_dir=TRAIN_DIR)
 prepare_data.filter_by_stddev(color_dir=TRAIN_COLOR_DIR,
                               grayscale_dir=TRAIN_DIR)
+
+generator_input = tf.placeholder(dtype=tf.float32, shape=[None, None, None, 1])
+
+real_data = tf.placeholder(dtype=tf.float32, shape=[None, None, None, 3])
