2b39958bf3b0e03e3e5ec25484388791b4d37e72
Ashutosh <30094761+agojiya@users.noreply.github.com>
2018-08-30 15:31:02 -0400

    Setting up for saving

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ BASE_DIR = path.join('X:', 'open-images-v4')
 TRAIN_COLOR_DIR = path.join(BASE_DIR, 'train')
 TRAIN_DIR = path.join(BASE_DIR, 'train_grayscale')
 
+SAVER_FORMAT = 'conv2d_3-%s-%s'
+
 prepare_data.convert_to_grayscale(color_dir=TRAIN_COLOR_DIR,
                                   grayscale_dir=TRAIN_DIR)
 prepare_data.filter_by_stddev(color_dir=TRAIN_COLOR_DIR,
@@ -20,3 +22,7 @@ color = tf.placeholder(dtype=tf.float32, shape=[None, None, None, 3])
 
 loss = tf.reduce_sum(tf.squared_difference(colorizer_out, color))
 optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+saver = tf.train.Saver(max_to_keep=None)
+with tf.Session() as session:
+    session.run(tf.global_variables_initializer())
