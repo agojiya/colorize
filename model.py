eb146755cc21026f456cdc50600edd40f9a9d26f
Ashutosh <30094761+agojiya@users.noreply.github.com>
2018-10-27 20:19:52 -0400

    Generator with convolution and transposed convolution

--- model.py
+++ model.py
@@ -1,33 +1,50 @@
 import tensorflow as tf
 
 CONSTANT_255 = tf.constant(255, dtype=tf.float32)
+CHANNEL_LABELS = ['R', 'G', 'B']
 
 
-def create_model(generator_input):
-    """ First attempt at a generator function. """
-    shape = tf.shape(generator_input)
-    red = create_model_layer(generator_input, [32, 8, 64, 16, 128, 32],
-                             'R')
-    green = create_model_layer(generator_input, [32, 8, 64, 16, 128, 32],
-                               'G')
-    blue = create_model_layer(generator_input, [32, 8, 64, 16, 128, 32],
-                              'B')
-    return tf.reshape(tf.stack([red, green, blue], axis=4),
-                      shape=[shape[0], shape[1], shape[2], 3])
+def create_model(grayscale_in):
+    conv_counts = [1, 1, 1]
+    kernel_sizes = [6, 4, 2]
 
-
-def create_model_layer(generator_input, structure, label):
-    """ Creates a conv2d network starting at <generator_input> according to
-    <structure> with label <label>.
-    Creates structure[i] conv2d layers with one filter and kernel size
-    structure[i+1] for 0<=i<=len(structure)/2. """
-    layers = [generator_input]
-    for i in range(int(len(structure) / 2)):
-        for layer_index in range(structure[2 * i]):
-            name = label + '-' + str(i) + '-' + str(layer_index)
-            layer = tf.layers.conv2d(inputs=layers[-1], filters=1,
-                                     kernel_size=structure[2 * i + 1],
-                                     activation=tf.nn.relu,
-                                     padding="same", name=name)
+    layers = [grayscale_in]
+    for i in range(len(conv_counts)):
+        count = conv_counts[i]
+        kernel_size = kernel_sizes[i]
+        for j in range(count):
+            layer = tf.layers.conv2d(inputs=layers[-1], filters=16,
+                                     kernel_size=kernel_size,
+                                     activation=tf.nn.relu, padding="same",
+                                     name="c2d-{}".format(i + j + 1))
             layers.append(layer)
-    return tf.multiply(layers[-1], CONSTANT_255)
+        pool_layer = tf.layers.max_pooling2d(inputs=layers[-1], pool_size=2,
+                                             strides=2, padding="same",
+                                             name="p-{}".format(i + 1))
+        layers.append(pool_layer)
+
+    kernel_sizes_reversed = list(reversed(kernel_sizes))
+    red_layers, green_layers, blue_layers = [layers[-1]], \
+                                            [layers[-1]], \
+                                            [layers[-1]]
+    channel_layers = [red_layers, green_layers, blue_layers]
+    for channel in range(3):
+        for i in range(len(conv_counts)):
+            prev_layer = channel_layers[channel][-1]
+            kernel_size = kernel_sizes_reversed[i]
+            layer_name = "c2dT-{}-{}".format(CHANNEL_LABELS[channel], i + 1)
+            layer = tf.layers.conv2d_transpose(inputs=prev_layer, filters=16,
+                                               kernel_size=kernel_size,
+                                               strides=2,
+                                               activation=tf.nn.relu,
+                                               padding="same",
+                                               name=layer_name)
+            channel_layers[channel].append(layer)
+        reduced_output = tf.reduce_sum(channel_layers[channel][-1], axis=3)
+        channel_layers[channel][-1] = reduced_output
+
+    output = [channel_layers[0][-1],
+              channel_layers[1][-1],
+              channel_layers[2][-1]]
+    stacked_output = tf.stack(output, axis=3)
+    return stacked_output
